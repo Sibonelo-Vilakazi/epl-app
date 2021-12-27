9a74580c9b584df01f560af76283eede937bc69f
Sibonelo-Vilakazi <sibonelo@monkeyandriver.com>
2021-12-27 21:42:15 +0200

    removing files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
+import { HomeComponent } from './pages/home/home.component';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  
+  path:'',component:HomeComponent
 }];
 
 @NgModule({
